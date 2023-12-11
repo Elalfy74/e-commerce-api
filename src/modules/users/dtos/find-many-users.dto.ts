1eccf102829e56c09da20cd56b113aca9f508230
elalfy74 <mahmoudelalfy13@gmail.com>
2023-12-12 00:18:37 +0200

    fix isAdmin query param type

--- src/modules/users/dtos/find-many-users.dto.ts
+++ src/modules/users/dtos/find-many-users.dto.ts
@@ -1,7 +1,9 @@
+import { Transform } from 'class-transformer';
 import { IsBoolean, IsOptional } from 'class-validator';
 import { PaginationAndFilterDto } from 'src/common';
 
 export class FindManyUsersDto extends PaginationAndFilterDto {
+  @Transform(({ obj }) => obj.isAdmin === 'true')
   @IsBoolean()
   @IsOptional()
   isAdmin?: boolean;
