993e7a56d471ea0a5d183a97b7a1eef3e0e11d31
elalfy74 <mahmoudelalfy13@gmail.com>
2023-12-13 00:55:29 +0200

    Refactor getWhereQuerySearch function to handle undefined querySearch and single field search

--- src/common/lib/utils.ts
+++ src/common/lib/utils.ts
@@ -1,4 +1,18 @@
-export function getWhereQuerySearch<T>(querySearch: string, fields: (keyof T)[]) {
+export function getWhereQuerySearch<T>(querySearch: string | undefined, fields: (keyof T)[]) {
+  if (!querySearch || fields.length === 0) {
+    return {};
+  }
+
+  // If there is only one field to search, we can use a simple contains query
+  if (fields.length === 1) {
+    return {
+      [fields[0]]: {
+        contains: querySearch,
+        mode: 'insensitive',
+      },
+    };
+  }
+
   return {
     OR: fields.map((field) => ({
       [field]: {
